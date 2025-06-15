b3f424419ca365903569ce1420a5e962723c8e3e
William Gu <wg2401@columbia.edu>
2025-06-15 00:31:24 -0500

    finished status

--- src/main/java/gurt/commands/Status.java
+++ src/main/java/gurt/commands/Status.java
@@ -3,11 +3,11 @@ package gurt.commands;
 import gurt.helperFunctions.*;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Status
@@ -19,8 +19,37 @@ public class Status
             Path dotGurtPath = projRootDir.resolve(".gurt");
             Path headPath = dotGurtPath.resolve("HEAD");
 
-            //get cur branch name
+            if (!Files.exists(headPath)) 
+            {
+                System.out.println("Error: no HEAD found. Did you run `gurt init`?");
+                return;
+            }
+
+            //get current commit and load it into map
             String headText = Files.readString(headPath);
+            String refsString = new String();
+            if (headText.startsWith("ref:")) 
+            {
+                refsString = headText.substring("ref:".length()).trim();
+            }
+            Path refsPath = dotGurtPath.resolve(refsString);
+
+            HashMap<Path, String> fileToHash = new HashMap<>();
+
+            //load latest commit blobs
+            if (Files.exists(refsPath))
+            {
+                String prevCom = Files.readString(refsPath).trim();
+                GurtFileHandler.loadCommit(fileToHash, prevCom, projRootDir);
+            }
+
+            //load files into lists
+            ArrayList<String> toCommit = new ArrayList<>();
+            ArrayList<String> unstaged = new ArrayList<>(); 
+            ArrayList<String> untracked = new ArrayList<>();
+            findHashes(projRootDir, toCommit, unstaged, untracked, projRootDir, fileToHash);
+
+            //get cur branch name
             String[] headParts = headText.split(" ");
             String branchPathString = headParts[0];
             Path branchAbsolutePath = projRootDir.resolve(branchPathString);
@@ -29,7 +58,47 @@ public class Status
             System.out.println("On branch " + branchName + ":");
             System.out.println();
 
+            System.out.println("Changes to be committed:");
+            if (toCommit.isEmpty())
+            {
+                System.out.println("    (none)");
+            }
+            else
+            {
+                for (String f : toCommit)
+                {
+                    System.out.println("    " + f);
+                }
+            }
+            System.out.println();
+
+            System.out.println("Changes not staged for commit:");
+            if (unstaged.isEmpty())
+            {
+                System.out.println("    (none)");
+            }
+            else
+            {
+                for (String f : unstaged)
+                {
+                    System.out.println("    " + f);
+                }
+            }
+            System.out.println();
             
+            System.out.println("Untracked files:");
+            if (untracked.isEmpty())
+            {
+                System.out.println("    (none)");
+            }
+            else
+            {
+                for (String f : untracked)
+                {
+                    System.out.println("    " + f);
+                }
+            }
+            System.out.println();
        }
        catch (IOException e)
        {
@@ -38,15 +107,18 @@ public class Status
     }
 
     //recurses thru all dirs and adds path names from index to respective lists
-    private void findHashes(Path curDir, ArrayList<String> toCommit, ArrayList<String> modified, ArrayList<String> untracked, Path projRootDir)
+    //pass in projRoot Dir (absolute path), so curDir will always be an absolute path
+    //ArrayLists contain relativized paths
+    private static void findHashes(Path curDir, ArrayList<String> toCommit, ArrayList<String> unstaged, ArrayList<String> untracked, Path projRootDir, HashMap<Path,String> fileToHash)
     {
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(curDir)) 
-        {
+        {   
+            //recurse to add to lists
             for (Path p : stream)
             {
                 if (Files.isDirectory(p))
                 {
-                    findHashes(p, toCommit, modified, untracked, projRootDir);
+                    findHashes(p, toCommit, unstaged, untracked, projRootDir, fileToHash);
                 }
                 
                 if (Files.isRegularFile(p))
@@ -65,11 +137,15 @@ public class Status
 
                         if (fileHash.equals(hash))
                         {
-                            toCommit.add(projRootDir.relativize(p).toString());
+                            String fileBlobInCommit = fileToHash.get(p);
+                            if (fileBlobInCommit == null || !fileHash.equals(fileBlobInCommit))
+                            {
+                                toCommit.add(projRootDir.relativize(p).toString());
+                            }
                         }
                         else
                         {
-                            modified.add(projRootDir.relativize(p).toString());
+                            unstaged.add(projRootDir.relativize(p).toString());
                         }
                     }
                 }
