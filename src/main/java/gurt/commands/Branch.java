2d000ea9f3148865ae5d508f8dbbc52cc6438228
William Gu <wg2401@columbia.edu>
2025-06-18 17:17:57 -0500

    made branchNew to create new branches, still need to handle detached head

--- src/main/java/gurt/commands/Branch.java
+++ src/main/java/gurt/commands/Branch.java
@@ -12,9 +12,68 @@ import java.util.Collections;
 
 public class Branch 
 {
-    public void branchNew()
+    public void branchNew(Path projRootDir, String branchName)
     {
+        try
+        {
+            Path dotGurtPath = projRootDir.resolve(".gurt");
+            Path headPath = dotGurtPath.resolve("HEAD");
+            Path branchesPath = dotGurtPath.resolve("refs").resolve("heads");
 
+            //get current commit:
+            String headText = Files.readString(headPath);
+            String refsString = new String();
+            
+            if (headText.startsWith("ref:")) 
+            {
+                refsString = headText.substring("ref:".length()).trim();
+            }
+            
+            Path refsPath = dotGurtPath.resolve(refsString);
+            String curCom = "";
+            
+            if (Files.exists(refsPath))
+            {
+                curCom = Files.readString(refsPath).trim();
+
+            }
+
+            Path newBranchPath = branchesPath.resolve(branchName);
+
+            //error checking:
+            //branch already exists
+            if (Files.exists(newBranchPath)) 
+            {
+                System.out.println("fatal: branch '" + branchName + "' already exists");
+                return;
+            }   
+
+            //branch name is used for an intermediate directory
+            if (Files.exists(newBranchPath) && Files.isDirectory(newBranchPath)) 
+            {
+                System.out.println("fatal: a directory named '" + branchName + "' already exists");
+                return;
+            }
+
+            //branch path has a file that blocks its intermediate directory
+            Path validDirCheck = newBranchPath.getParent();
+            while (!validDirCheck.equals(branchesPath))
+            {
+                if (Files.isRegularFile(validDirCheck))
+                {
+                    System.out.println("fatal: " + validDirCheck + "' is not a directory");
+                    return;
+                }
+            }
+
+            Files.createDirectories(newBranchPath.getParent());
+            Files.writeString(newBranchPath, curCom);
+
+        }
+        catch(IOException e)
+        {
+
+        }
     }
 
     public static void branchList(Path projRootDir)
